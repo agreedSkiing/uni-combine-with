b0efb144fbc9502b347c208ce33ad182e8fe9454
agreedSkiing <easen@mail.se>
2023-04-06 18:01:43 +0200

    Prettier config
    Signed-off-by: agreedSkiing <easen@mail.se>

--- combined/src/test/java/se/agreedskiing/uni/combined/with/tests/CombinedWithTest.java
+++ combined/src/test/java/se/agreedskiing/uni/combined/with/tests/CombinedWithTest.java
@@ -1,6 +1,7 @@
 package se.agreedskiing.uni.combined.with.tests;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,8 +10,29 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class CombinedWithTest {
 
+    @Nested
+    class using_record_ {
+
+        @Test
+        void constructor() {
+            given()
+                    .when().get("/hello")
+                    .then()
+                    .statusCode(200)
+                    .body(is("Hello RESTEasy"));
+                }
+                @Test
+                void methods() {
+                    given()
+                            .when().get("/hello")
+                            .then()
+                            .statusCode(200)
+                            .body(is("Hello RESTEasy"));
+                }
+    }
+    
     @Test
-    public void testHelloEndpoint() {
+    public void using_object_constructor() {
         given()
           .when().get("/hello")
           .then()
