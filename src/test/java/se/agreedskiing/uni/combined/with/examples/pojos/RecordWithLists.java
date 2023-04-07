7d477a582f0f84cf69a142cc0951716cbe28b043
agreedSkiing <easen@mail.se>
2023-04-07 20:52:53 +0200

    Rename again
    Signed-off-by: agreedSkiing <easen@mail.se>

--- src/test/java/se/agreedskiing/uni/combined/with/examples/pojos/RecordWithLists.java
+++ src/test/java/se/agreedskiing/uni/combined/with/examples/pojos/RecordWithLists.java
@@ -2,16 +2,16 @@ package se.agreedskiing.uni.combined.with.examples.pojos;
 
 import java.util.List;
 
-public record ListRecord(
+public record RecordWithLists(
   List<Integer> numbers,
   List<String> texts,
   List<Boolean> booleans
 ) {
-  public static ListRecord of(
+  public static RecordWithLists of(
     final List<Integer> numbers,
     final List<String> texts,
     final List<Boolean> booleans
   ) {
-    return new ListRecord(numbers, texts, booleans);
+    return new RecordWithLists(numbers, texts, booleans);
   }
 }
